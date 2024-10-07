6fc8d1d75fd297a063f4f64aa3e1d30f5f6c31c0
root <root@localhost>
2024-10-07 21:09:04 +0800

    auto publish 1728306542054

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+    const fruits = [
+        
+        { stock:1, name: "Dragon", price: 200 },
+        { stock:1, name: "Kitsune", price: 200},
+        { stock:1, name: "Leopard", price: 200},
+        { stock:1, name: "Dough", price: 200},
+        { stock:1, name: "T-Rex", price: 200},
+        { stock:1, name: "Mammoth", price: 200},
+        { stock:1, name: "Spirit", price: 200},
+        { stock:1, name: "Venom", price: 200},
+        { stock:1, name: "Shadow", price: 200},
+        { stock:1, name: "Control", price: 200},
+        { stock:1, name: "Rumble", price: 200},
+        { stock:1, name: "Buddha", price: 200},
+        { stock:1, name: "Portal", price: 200},
+        { stock:1, name: "Blizzard", price: 200},
+        { stock:1, name: "Phoenix", price: 200},
+        { stock:1, name: "Sound", price: 200},
+        { stock:1, name: "Love", price: 200},
+       
+    ];
+
+    function generateTable(fruits) {
+        const container = document.getElementById('table-container');
+        let table = '<table class="table table-bordered border-primary"><tr><th>Stock</th><th>Fruit Name</th><th>Price</th></tr>';
+
+        fruits.forEach(fruit => {
+            table += `<tr><td class="text-center">${fruit.stock}</td><td> <img src="fruits/${fruit.name}.Fruit.png" class="avatar"> ${fruit.name}</td><td>${fruit.price}</td></tr>`;
+        });
+
+        table += '</table>';
+        container.innerHTML = table;
+    }
+
+    generateTable(fruits);
