3eb4d2c7c8121e1618ca911e74e38a1df7796e75
root <root@localhost>
2024-10-08 23:45:42 +0800

    auto publish 1728402340496

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
-
 const titles = document.querySelector("#output")
 
 
     const fruits = [
-        
-        { stock:"Unavailable", name: "Dragon", price: 190},
-        { stock:1, name: "Kitsune", price: 190},
-        { stock:"Unavailable", name: "Leopard", price: 70},
-        { stock:1, name: "Dough", price: 60},
-        { stock:"Unavailable", name: "T-Rex", price: 50},
-        { stock:1, name: "Mammoth", price: 40},
-        { stock:"Unavailable", name: "Spirit", price: 30},
-        { stock:1, name: "Venom", price: 25},
-        { stock:2, name: "Shadow", price: 25},
-        { stock:2, name: "Control", price: 30},
-        { stock:2, name: "Rumble", price: 25},
-        { stock:2, name: "Buddha", price: 30},
-        { stock:1, name: "Portal", price: 30},
-        { stock:2, name: "Blizzard", price: 15},
-        { stock:2, name: "Phoenix", price: 10},
-        { stock:1, name: "Sound", price: 10},
-        { stock:1, name: "Love", price: 5},
-        { stock:2, name: "Gravity", price: "--"},
-        { stock:1, name: "Pain", price: "--"},
-       { stock:1, name: "Spider", price: "--"},
-       { stock:1, name: "Quake", price: "--"},
-       { stock:2, name: "Magma", price: "--"},
-       { stock:2, name: "Ghost", price: "--"},
-       { stock:1, name: "Barrier", price: "--"},
-       { stock:1, name: "Rubber", price: "--"},
-       { stock:2, name: "Light", price: "--"},
-       { stock:1, name: "Diamond", price: "--"},
-       { stock:1, name: "Dark", price: "--"},
-       { stock:1, name: "Sand", price: "--"},
-       { stock:2, name: "Ice", price: "--"},
-       { stock:1, name: "Flame", price: "--"},
-       { stock:1, name: "Spike", price: "--"},
-       { stock:1, name: "Smoke", price: "--"},
-       { stock:2, name: "Bomb", price: "--"},
-       { stock:2, name: "Spring", price: "--"},
-       { stock:2, name: "Chop", price: "--"},
-       { stock:2, name: "Spin", price: "--"},
-       { stock:2, name: "Rocket", price:"--"}
+
+        { stock:0,  name: "Dragon",      price: 190,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Kitsune",      price: 190,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:0,  name: "Leopard",    price: 70,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Dough",        price: 60,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:0,  name: "T-Rex",       price: 50,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Mammoth",  price: 40,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:0,  name: "Spirit",        price: 30,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Venom",      price: 25,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Shadow",    price: 25,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Control",     price: 30,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Rumble",     price: 25,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Buddha",     price: 30,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Portal",       price: 30,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Blizzard",    price: 15,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Phoenix",     price: 10,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Sound",        price: 10,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Love",          price: 5,igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:2,  name: "Gravity",     price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+        { stock:1,  name: "Pain",          price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Spider",        price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Quake",        price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Magma",      price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Ghost",         price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Barrier",      price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Rubber",      price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Light",         price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Diamond",    price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Dark", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Sand", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Ice", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Flame", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Spike", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:1,  name: "Smoke", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Bomb", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Spring", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Chop", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Spin", price: "--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"},
+       { stock:2,  name: "Rocket", price:"--",igPrice:00,fval:00,type:"Unknown",rarity:"Unknown"}
     ];
     /*
     const fruits = [
@@ -56,14 +55,16 @@ const titles = document.querySelector("#output")
         <h1 class="text-center">BenNBren's Inventory</h1>
         <table class="table table-bordered table-info">
         <tr>
-            <th colspan="5" class="text-center">PHYSICAL FRUITS</th>
+            <th colspan="7" class="text-center">PHYSICAL FRUITS</th>
         </tr>
                                 <tr>
                                     <th>Stock</th>
                                     <th>Fruit</th>
-                                    <th>Price</th>
+                                    <th>Sale Price</th>
                                     <th>In-Game Price</th>
                                     <th>Fruit Value</th>
+                                    <th>Type</th>
+                                    <th>Rarity</th>
                                 </tr>`;
 
         fruits.forEach(fruit => {
@@ -71,12 +72,15 @@ const titles = document.querySelector("#output")
                                      <td class="text-center">${fruit.stock}</td>
                                      <td><img src="./fruits/${fruit.name}Fruit.png"class="avatar"> ${fruit.name}</td>
                                      <td class="text-right">₱${fruit.price}.00</td>
-                                     <td class="text-right">$0.00</td>
-                                     <td class="text-right">$0.00</td>
+                                     <td class="text-right">${fruit.igPrice}</td>
+                                     <td class="text-right">${fruit.fval}</td>
+                                     <td>${fruit.type}</td>
+                                     <td>${fruit.rarity}</td>
+                                     
                                      
                                      
                                </tr>`;
-                              
+
         });
 
         table += '</table>';
@@ -84,7 +88,7 @@ const titles = document.querySelector("#output")
     }
 
     generateTable(fruits);
- 
+
  //Date
  /*
 let currentDateTime = new Date();
@@ -126,35 +130,12 @@ const upDates = document.querySelector("#upDate")
             let formattedDateTime = currentDateTime.toLocaleString('en-US', options);
             return formattedDateTime;
         }
-        
+
         function updateDateTime() {
             document.querySelector("#upDate")
             .innerText = "As of: "+getNewDateTime();
         }
-        
+
         setInterval(updateDateTime, 1000);
         updateDateTime();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
